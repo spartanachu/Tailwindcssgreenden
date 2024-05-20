b06687738bb44ff6fbacf74e51d64a61fcada176
spartanachu <achuthakumar1151@gmail.com>
2024-05-20 21:00:40 +0530

    new code updated 20-05-2024 new

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ menuicons.addEventListener("click",function()
 closes.addEventListener("click",function(){
     sidenav.style.right="-50%";
 })
+
+
+// Search Box Query
+
+var search=document.getElementById("search")
+var productcontainer=document.getElementById("Product-container")
+var productlist=productcontainer.querySelectorAll("div")
+
+// console.log(productlist)
+
+search.addEventListener("keyup",function(){
+    var searchlist=event.target.value.toUpperCase()
+    for(count=0;count<productlist.length;count=count+1)
+        {
+            var productname=productlist[count].querySelector("h1").textContent
+            console.log(productname)
+            if(productname[count].toUpperCase().indexOf(searchlist)<0)
+                {
+                    productlist[count].style.display="none"
+                }
+                else
+                {
+                    productlist[count].style.display="block"
+                }
+        }
+})
